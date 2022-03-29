b65bdd6bea5de7dc3cab51ba0ee721a898e1ba65
KartikJain-Zemoso <kartik.jain@zemosolabs.com>
2022-03-29 10:22:10 +0530

    Test cases

--- src/test/java/com/example/crudDemo/dao/EmployeeRepositoryTest.java
+++ src/test/java/com/example/crudDemo/dao/EmployeeRepositoryTest.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
