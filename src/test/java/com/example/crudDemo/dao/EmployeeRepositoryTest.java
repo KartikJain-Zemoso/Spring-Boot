42d9083f38fc693a8c97ca19ea45f1730321f47f
KartikJain-Zemoso <kartik.jain@zemosolabs.com>
2022-03-30 09:46:00 +0530

    Test

--- src/test/java/com/example/crudDemo/dao/EmployeeRepositoryTest.java
+++ src/test/java/com/example/crudDemo/dao/EmployeeRepositoryTest.java
@@ -3,10 +3,10 @@ package com.example.crudDemo.dao;
 import com.example.crudDemo.entity.Employee;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -55,7 +55,7 @@ class EmployeeRepositoryTest {
     @Order(4)
     @Rollback(value = false)
     public void updateEmployeeTest(){
-        Employee employee = employeeRepository.findById(12).get();
+        Employee employee = employeeRepository.findById(13).get();
         employee.setEmail("ram@gmail.com");
       Employee updatedEmployee = employeeRepository.save(employee);
         org.assertj.core.api.Assertions.assertThat(updatedEmployee.getEmail()).isEqualTo("ram@gmail.com");
@@ -64,7 +64,7 @@ class EmployeeRepositoryTest {
     @Order(5)
     @Rollback(value = false)
     public void deleteEmployeeTest(){
-        Employee employee = employeeRepository.findById(12).get();
+        Employee employee = employeeRepository.findById(13).get();
          employeeRepository.delete(employee);
          Employee employee1 = null;
 
